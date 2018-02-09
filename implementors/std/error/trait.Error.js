43c094d38eed0bbe2ab395aabe4d8a9f114ac77c
Travis CI User <travis@example.org>
2018-02-09 17:57:16 +0000

    Documentation for netvl/xml-rs@dc9bb43

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["xml"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"xml/reader/struct.Error.html\" title=\"struct xml::reader::Error\">Error</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"xml/writer/enum.Error.html\" title=\"enum xml::writer::Error\">EmitterError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
